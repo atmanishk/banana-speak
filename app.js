4dafaba6d55537d721d7b3a7f77b2a6360cb233d
Manish Kumar <manish.mk0195@gmail.com>
2021-09-08 12:51:19 +0530

    second change

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-// var serverURL = "https://api.funtranslations.com/translate/minion.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationURL(input) {
     return serverURL + "?" + "text=" + input
@@ -12,7 +12,7 @@ function getTranslationURL(input) {
 
 function errorHandler(error) {
     console.log("error occured", error);
-    alert("Something wrong with server! try again after some time");
+    alert("Something wrong with server! try again after some time")
 }
 
 function clickHandler() {
@@ -24,7 +24,7 @@ function clickHandler() {
         var translatedText = json.contents.translated;
         outputDiv.inerText = translatedText;
     })
-    .catch(errorHandler);
+    .catch(errorHandler)
 }
 
-btnTranslate.addEventListener("click", clickHandler);
+btnTranslate.addEventListener("click", clickHandler)
